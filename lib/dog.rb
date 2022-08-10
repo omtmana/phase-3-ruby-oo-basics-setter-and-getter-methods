a5e8b26564fa20b2f508da19f9ff0e647be900e7
Olivia <oliviatmanalastas@gmail.com>
2022-08-10 10:56:39 -0400

    Done with assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,42 @@
 class Dog
+   # :name setter method
+   def name=(dog_name)
+      @name = dog_name
+   end
+   # :name getter method
+   def name
+      @name
+   end
 
+   # :breed setter method
+   def breed=(dog_breed)
+      @breed = dog_breed
+   end
+
+   # :breed getter method
+   def breed
+      @breed
+   end
+end
+
+class Person
+   # :name setter method
+   def name=(name_getter)
+      @name = name_getter
+   end
+
+   # :name getter method
+   def name
+      @name
+   end
+
+   # :job setter method
+   def job=(job_getter)
+      @job = job_getter
+   end
+
+   # :job getter method
+   def job
+      @job
+   end
 end
